e2dda0bd6f2034ccf9725e8bbf273697e55a4ab6
chrisjwalker <8091191+chrisjwwalker@users.noreply.github.com>
2023-10-11 16:10:51 +0100

    Initial

---
+++ SwiftUIExamples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIExamples
+//
+//  Created by Chris J W Walker on 11/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIExamples/SwiftUIExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExamplesApp.swift
+//  SwiftUIExamples
+//
+//  Created by Chris J W Walker on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
